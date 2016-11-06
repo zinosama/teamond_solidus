7d2fe0fea599e11102f977a44de10af04d35cd77
Zino <rhu5@u.rochester.edu>
2016-11-06 10:12:39 -0600

    Init rubocop fix (#2)
    * Add migrate folder to rubocop ignore
    
    * Init rubocop fix

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,15 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Teamond
+
   class Application < Rails::Application
-    
+
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
@@ -24,5 +25,7 @@ module Teamond
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
   end
+
 end
